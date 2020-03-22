826f919fbc42f04774874b656ad287b370a4f7fd
Valeriia <valeria.chyornaya@gmail.com>
2020-03-22 23:38:14 +0200

    add Code Splitting Lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Layout from './Layout';
-import Homepage from '../views/Homepage';
-import MoviesPage from '../views/MoviesPage';
-import MovieDetailsPage from '../views/MovieDetailsPage';
-import NotFound from '../views/NotFound';
 import routes from '../routes';
 
+const Homepage = lazy(() => import('../views/Homepage'));
+const MovieDetailsPage = lazy(() => import('../views/MovieDetailsPage'));
+const MoviesPage = lazy(() => import('../views/MoviesPage'));
+const NotFound = lazy(() => import('../views/NotFound'));
+
 const App = () => (
-  <Layout>
-    <Switch>
-      <Route path={routes.HOMEPAGE} exact component={Homepage} />
-      <Route path={routes.MOVIES} exact component={MoviesPage} />
-      <Route path={routes.MOVIE_DETAILS_PAGE} component={MovieDetailsPage} />
-      <Route component={NotFound} />
-      {/* <Redirect to={routes.homepage} /> */}
-    </Switch>
-  </Layout>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Layout>
+      <Switch>
+        <Route path={routes.HOMEPAGE} exact component={Homepage} />
+        <Route path={routes.MOVIES} exact component={MoviesPage} />
+        <Route path={routes.MOVIE_DETAILS_PAGE} component={MovieDetailsPage} />
+        <Route component={NotFound} />
+        {/* <Redirect to={routes.homepage} /> */}
+      </Switch>
+    </Layout>
+  </Suspense>
 );
 
 export default App;
--- src/views/MovieDetailsPage/MovieDetailsPage.js
+++ src/views/MovieDetailsPage/MovieDetailsPage.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import MovieDetails from '../../components/MovieDetails';
 import { Link, Switch, Route } from 'react-router-dom';
-import Cast from '../Cast';
-import Reviews from '../Reviews';
+// import Cast from '../Cast';
+// import Reviews from '../Reviews';
 import Notification from '../../components/Notification';
 import Spiner from '../../components/Spiner';
 import routes from '../../routes';
 import movieApi from '../../services/movieApi';
+
+const Cast = lazy(() => import('../Cast'));
+const Reviews = lazy(() => import('../Reviews'));
 
 class MoviesPage extends Component {
   state = {
@@ -47,62 +50,64 @@ class MoviesPage extends Component {
     const { match } = this.props;
     const { error, loading } = this.state;
     return (
-      <>
-        <button onClick={this.onBackButton} type="button">
-          Go back
-        </button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <>
+          <button onClick={this.onBackButton} type="button">
+            Go back
+          </button>
 
-        {error && (
-          <Notification
-            message={`Whoops, something went wrong: ${error.message}`}
-          />
-        )}
+          {error && (
+            <Notification
+              message={`Whoops, something went wrong: ${error.message}`}
+            />
+          )}
 
-        {loading && <Spiner />}
+          {loading && <Spiner />}
 
-        {this.state.movieDetail && (
-          <MovieDetails
-            url={`${movieApi.IMG_URL}${this.state.movieDetail.backdrop_path}`}
-            title={this.state.movieDetail.title}
-            popularity={this.state.movieDetail.popularity}
-            overview={this.state.movieDetail.overview}
-            genres={this.state.movieDetail.genres}
-          />
-        )}
+          {this.state.movieDetail && (
+            <MovieDetails
+              url={`${movieApi.IMG_URL}${this.state.movieDetail.backdrop_path}`}
+              title={this.state.movieDetail.title}
+              popularity={this.state.movieDetail.popularity}
+              overview={this.state.movieDetail.overview}
+              genres={this.state.movieDetail.genres}
+            />
+          )}
 
-        <hr />
+          <hr />
 
-        <p>Addition Information</p>
-        <ul>
-          <li>
-            <Link
-              to={{
-                pathname: `${match.url}/cast`,
-                state: { from: this.props.location.state.from },
-              }}
-            >
-              Cast
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={{
-                pathname: `${match.url}/reviews`,
-                state: { from: this.props.location.state.from },
-              }}
-            >
-              Reviews
-            </Link>
-          </li>
-        </ul>
+          <p>Addition Information</p>
+          <ul>
+            <li>
+              <Link
+                to={{
+                  pathname: `${match.url}/cast`,
+                  state: { from: this.props.location.state.from },
+                }}
+              >
+                Cast
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={{
+                  pathname: `${match.url}/reviews`,
+                  state: { from: this.props.location.state.from },
+                }}
+              >
+                Reviews
+              </Link>
+            </li>
+          </ul>
 
-        <hr />
+          <hr />
 
-        <Switch>
-          <Route path={routes.CAST} component={Cast} />
-          <Route path={routes.REVIEWS} component={Reviews} />
-        </Switch>
-      </>
+          <Switch>
+            <Route path={routes.CAST} component={Cast} />
+            <Route path={routes.REVIEWS} component={Reviews} />
+          </Switch>
+        </>
+      </Suspense>
     );
   }
 }
